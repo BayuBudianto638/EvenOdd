2cb5eba63c7d8e9842609dbab0af4edd7091bf03
Bayu <radamanthys_ok@yahoo.com>
2022-08-24 15:14:55 +0700

    Add project files.

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var l = 2;
+var r = 5;
+var result = new List<int>();
+for (int i = l; l<=r;l++)
+{
+    if (i % 2 != 0)
+        result.Add(i);
+}
+
+Console.WriteLine($"Result {result}");
+Console.ReadKey();
